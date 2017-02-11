fa594641e11ac3d9972b5fc022976e41fe94bbac
Joerg Zimmermann <comtel2000@gmail.com>
2017-02-11 19:09:42 +0100

    remove demo loc

--- src/main/java/org/comtel2000/fritzhome/skill/FritzHomeSpeechletRequestStreamHandler.java
+++ src/main/java/org/comtel2000/fritzhome/skill/FritzHomeSpeechletRequestStreamHandler.java
@@ -14,7 +14,7 @@ import com.amazon.speech.speechlet.lambda.SpeechletRequestStreamHandler;
 public final class FritzHomeSpeechletRequestStreamHandler extends SpeechletRequestStreamHandler {
   private static final Set<String> supportedApplicationIds = new HashSet<>();
   static {
-    supportedApplicationIds.add("amzn1.ask.skill.69ed8a07-ba0f-4ca0-886e-ea2ca01a2e77");
+    supportedApplicationIds.add("amzn1.ask.skill.11111111111-1111111111");
   }
 
   public FritzHomeSpeechletRequestStreamHandler() {
